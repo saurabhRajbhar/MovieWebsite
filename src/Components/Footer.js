cbe9584f67bd80b829f174fbd9bb7724cf11a8d0
Saurabh Raj <saurabhrajs9898@gmail.com>
2024-04-30 10:10:59 +0530

    save change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,26 +2,26 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-4">
             <div className="container">
-                <div className="flex justify-between">
-                    <div className="w-full sm:w-auto mb-3">
+                <div className="flex flex-col lg:flex-row justify-between">
+                    <div className="w-full sm:w-1/2 lg:w-auto mb-3">
                         <div>About Us</div>
-                        <h1>Lorem ipsum dolor sit amet,</h1>
-                        <h1>consectetur adipiscing elit.</h1>
+                        <h1 className="text-sm mb-1">Lorem ipsum dolor sit amet,</h1>
+                        <h1 className="text-sm mb-1">consectetur adipiscing elit.</h1>
                     </div>
-                    <div className="w-full sm:w-auto mb-3">
+                    <div className="w-full sm:w-1/2 lg:w-auto mb-3">
                         <div>Contact Us</div>
-                        <h1>Email: example@example.com</h1>
-                        <h1>Phone: 123-456-7890</h1>
+                        <h1 className="text-sm mb-1">Email: example@example.com</h1>
+                        <h1 className="text-sm mb-1">Phone: 123-456-7890</h1>
                     </div>
-                    <div className="w-full sm:w-auto mb-3">
+                    <div className="w-full sm:w-1/2 lg:w-auto mb-3">
                         <div>Privacy Policy</div>
-                        <h1>Lorem ipsum dolor sit amet,</h1>
-                        <h1>consectetur adipiscing elit.</h1>
+                        <h1 className="text-sm mb-1">Lorem ipsum dolor sit amet,</h1>
+                        <h1 className="text-sm mb-1">consectetur adipiscing elit.</h1>
                     </div>
-                    <div className="w-full sm:w-auto mb-3">
+                    <div className="w-full sm:w-1/2 lg:w-auto mb-3">
                         <div>Terms and Conditions</div>
-                        <h1>Lorem ipsum dolor sit amet,</h1>
-                        <h1>consectetur adipiscing elit.</h1>
+                        <h1 className="text-sm mb-1">Lorem ipsum dolor sit amet,</h1>
+                        <h1 className="text-sm mb-1">consectetur adipiscing elit.</h1>
                     </div>
                 </div>
             </div>
